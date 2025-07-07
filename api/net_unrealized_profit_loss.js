f9ea2fc2050a9839c8b8543f4971d1d9177a4b2f
sagecrypto93 <sage.crypto93@outlook.com>
2025-07-07 12:18:04 +0100

    Update net_unrealized_profit_loss.js

--- api/net_unrealized_profit_loss.js
+++ api/net_unrealized_profit_loss.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const token = "0d866412-d29c-41ed-a044-7be4fb52478a"; // Your API key here
+  const token = "0d866412-d29c-41ed-a044-7be4fb52478a"; // Your API key
   const endpoint = `https://api.researchbitcoin.net/v1/net_unrealized_profit_loss/net_unrealized_profit_loss?token=${token}&date_field=2023-01-01&output_format=json`;
 
   try {
