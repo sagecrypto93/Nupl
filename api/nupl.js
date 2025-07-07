5f4ccfd84aeb38e6dd1f2512669b4049111211b0
sagecrypto93 <sage.crypto93@outlook.com>
2025-07-07 12:04:34 +0100

    Update nupl.js

--- api/nupl.js
+++ api/nupl.js
@@ -1,12 +1,11 @@
 export default async function handler(req, res) {
-  const token = "0d866412-d29c-41ed-a044-7be4fb52478a";
+  const token = "0d866412-d29c-41ed-a044-7be4fb52478a"; // Your API key here
   const endpoint = `https://api.researchbitcoin.net/v1/net_unrealized_profit_loss/net_unrealized_profit_loss?token=${token}&date_field=2023-01-01&output_format=json`;
 
   try {
     const response = await fetch(endpoint);
     if (!response.ok) {
-      res.status(response.status).json({ error: "Failed to fetch API data" });
-      return;
+      return res.status(response.status).json({ error: "Failed to fetch API data" });
     }
     const data = await response.json();
     res.setHeader("Access-Control-Allow-Origin", "*");
